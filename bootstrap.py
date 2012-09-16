6d1ec0a551955bb18b42d2da99620b837b6c5e37
tsing <tsing@jianqing.org>
2012-09-16 14:30:20 +0800

    use gevent

--- bootstrap.py
+++ bootstrap.py
@@ -1,4 +1,13 @@
 from domains import app
 from middleware import MethodRewrite
+from bottle import run
+import sys
 
 app = MethodRewrite(app)
+
+def main():
+    port = sys.argv[1]
+    run(server="gevent", port=port)
+
+if __name__ == '__main__':
+    main()
